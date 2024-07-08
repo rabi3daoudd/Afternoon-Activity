59c790168fe5f6b8a2b6587099f24a016be9fbc7
Joseph Mansour <joemansour2003@gmail.com>
2024-07-07 23:55:00 -0400

    feat: Dynamically run local settings and production settings

---
+++ camp_t/local_settings.py
@@ -0,0 +1,4 @@
+from camp_t.base_settings import *
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
+SECURE_SSL_REDIRECT = False
---
+++ camp_t/production_settings.py
@@ -0,0 +1,4 @@
+from camp_t.base_settings import *
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
