4f86c579dadeb0b1792606ec16690bcb16de8b2a
Joseph Mansour <joemansour2003@gmail.com>
2024-07-27 15:41:24 -0400

    fix: smaller requirements.txt

--- camp_t/base_settings.py
+++ camp_t/base_settings.py
@@ -81,16 +81,16 @@ WSGI_APPLICATION = 'camp_t.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.config(
+    DATABASES['default'] = {
+        dj_database_url.config(
             conn_max_age=500,
             conn_health_checks=True
         )
